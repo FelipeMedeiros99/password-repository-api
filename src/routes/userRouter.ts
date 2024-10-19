23abc75910fe87713665d022883289b05533d9ef
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2024-10-19 20:11:33 -0300

    feat: password encrypted and user data saved in the database

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -3,21 +3,26 @@ import { schemaValidation } from "../middlewares/validationMiddlewares";
 import { registerUserSchema } from "../schemas/userSchemas";
 import { createUserRepository, findUserReposytory } from "../repositories/userRepository";
 import { conflitError } from "../services/customErros";
+import { encryptPasswordService } from "../services/userServices";
 
 
 
 const userRouter = Router();
 
 userRouter.post("/register", schemaValidation(registerUserSchema), async (req: Request, res: Response)=>{
-    let {body: userData} = req;
+    const {body: userData} = req;
 
     const userExist = await findUserReposytory(userData);
 
     conflitError(Boolean(userExist));
+        
+    const encryptedPassword = await encryptPasswordService(userData.password)
     
-    // await createUserRepository(userData);
+    delete userData.passwordConfirmation
 
-    res.sendStatus(200);
+    await createUserRepository({...userData, password: encryptedPassword});
+
+    res.sendStatus(201);
 })
 
 export default userRouter;
--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,3 +1,13 @@
-export function encryptPassword(password:string){
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+export async function encryptPasswordService(password:string){
+    const saltRounds:number = Number(process.env.SALTS);
     
+    const encryptedPassword = await bcrypt.hash(password, saltRounds);
+
+    return encryptedPassword;
+
 }
