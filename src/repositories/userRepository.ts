1b09566ad42e6b4c2b8c296623863b7da5a47996
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2024-10-19 19:39:58 -0300

    feat: findUserRepository created

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,6 +1,11 @@
 import prisma from "../config";
 import { UserDataReceived } from "../types/userTypes";
 
+export async function findUserReposytory(userData: UserDataReceived) {
+    const user = await prisma.user.findFirst({ where: { email: userData.email } });
+    return user;
+}
+
 export async function createUserRepository(userData: UserDataReceived) {
-    await prisma.user.create({data: userData})
+    await prisma.user.create({ data: userData });
 }
