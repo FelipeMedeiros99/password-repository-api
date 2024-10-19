6614c888fc47bf1e93711c376c5cee98cabe9d00
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2024-10-19 15:02:47 -0300

    fix: required() added at registerUserSchema

--- src/schemas/userSchemas.ts
+++ src/schemas/userSchemas.ts
@@ -1,8 +1,8 @@
 import Joi from "joi";
 
 const registerUserSchema = Joi.object({
-    email: Joi.string().email().required,
-    password: Joi.string().required,
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
     passwordConfirmation: Joi.ref('password')
 });
 
