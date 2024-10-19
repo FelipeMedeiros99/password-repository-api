e73d29784be956a7b24446ac60b5d9fd66166689
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2024-10-19 17:31:20 -0300

    feat: errorHandler and userRouter added at main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import userRouter from "./userRouter";
+import { errorHandler } from "../middlewares/errorHandler.js";
 
 const router = Router();
+
+router.use(userRouter);
+router.use(errorHandler);
 
 export default router;
