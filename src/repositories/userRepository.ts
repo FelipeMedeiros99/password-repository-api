06ee70c169d7bb52c14088b8b560fd935262f58f
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2024-10-19 18:04:50 -0300

    refactor: function renamed

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,6 +1,6 @@
 import prisma from "../config";
 import { UserDataReceived } from "../types/userTypes";
 
-export async function createUser(userData: UserDataReceived) {
+export async function createUserRepository(userData: UserDataReceived) {
     await prisma.user.create({data: userData})
 }
