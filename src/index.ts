86fbff03081c08acf1367a20aa7b99c21d3d64db
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2024-10-19 12:23:10 -0300

    feat: main router created

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import express from "express";
+import express, { json } from "express";
+import router from "./routes/index";
 
 const app = express();
+app.use(json())
 
+app.use(router)
 
 app.listen(5000, ()=>console.log("app running at port 5000"))
 
