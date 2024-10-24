7be00a1c3cdef757414955e66429f7bed53556ee
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2024-10-23 23:22:34 -0300

    fix: unique at prisma title removed

---
+++ prisma/migrations/20241024022021_unique_removed_at_title/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "cards_title_key";
+
+-- DropIndex
+DROP INDEX "credentials_title_key";
+
+-- DropIndex
+DROP INDEX "securityNotes_title_key";
+
+-- DropIndex
+DROP INDEX "wifi_title_key";
--- src/services/storeServices.ts
+++ src/services/storeServices.ts
@@ -6,9 +6,9 @@ import { PathName } from "../types/userTypes";
 
 export async function validIfTitleExistsService(pathName: PathName, decryptedToken: DecryptedToken, title: string) {
     const titleExists = await findIfTitleExistsRepository(decryptedToken, pathName, title);
-    
-    if(titleExists){
-        const messageError: MessageError = {message: "this title existe", status: 409}
+
+    if (titleExists) {
+        const messageError: MessageError = { message: "this title already exists", status: 409 }
         throw messageError;
     }
 }
